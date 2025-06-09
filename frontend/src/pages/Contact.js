79890e185d3439409619ffa9868a573ba1a586ef
Dilek Gül <110824862+dilekgl@users.noreply.github.com>
2025-06-09 23:54:25 +0300

    Update Contact.js

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -18,7 +18,7 @@ const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:5000/send", {
+      const res = await fetch("http://anchmarine.vercel.app/send", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
